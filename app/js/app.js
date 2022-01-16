de559e0c26bb53a39242fb0dbd1ab64dc6b9719f
Elmer Mendieta <emendieta60236@ufide.ac.cr>
2022-01-15 19:05:43 -0600

    Filter Items [Completed]

--- app/js/app.js
+++ app/js/app.js
@@ -5,59 +5,118 @@ const ul = document.querySelector('ul');
 const list__description = document.querySelector('.list__description');
 const desktop__states = document.querySelector('.desktop__states');
 const check__icon = document.querySelector('span.check__icon');
+const states__list = document.querySelector('.states__list');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let icon = event.target.children[0].children[0];
-    let iconBoolean = false;
-    icon.classList.contains('checked') ? iconBoolean = true : iconBoolean = false;
-    if (input.value !== '') listAddTodo(input.value, iconBoolean);
-});
-
-const listAddTodo = (value, iconBoolean) => {
-    const li = document.createElement('li');
-
-    li.innerHTML = `<span class="check__icon ${iconBoolean ? 'checked' : ''}">
-                        <img style="${iconBoolean ? 'visibility: visible' : 'visibility: hidden'}" src="app/images/icon-check.svg" alt="">
-                    </span>
-                    <a href="#">${iconBoolean ? `<s style="color: hsl(233, 14%, 35%); font-weight: normal">${value}</s>`: value}</a>
-                    <i>
-                        <img src="app/images/icon-cross.svg" alt="icon-cross">
-                    </i>`;
-    ul.append(li);    
-    
-    input.value = '';
-
-    li.querySelector('span.check__icon').addEventListener('click', (event) => {
-        if (!li.querySelector('span.check__icon').classList.contains('checked')) {
-            li.querySelector('span.check__icon').classList.add('checked');
-            li.querySelector('span.check__icon').children[0].style.visibility = 'visible';
-        } else {
-            li.querySelector('span.check__icon').classList.remove('checked');
-            li.querySelector('span.check__icon').children[0].style.visibility = 'hidden';
-        }
-    });
-
-    listTodo();
-};
-
-const listTodo = () => { 
-    let listArrayTodo = Array.from(ul.children).map(node => node.innerText);
-    let span = desktop__states.querySelector('span');
-    let spanMobile = list__description.querySelector('span');
-
-    span.innerText = `${listArrayTodo.length} items left`;
-    spanMobile.innerText = `${listArrayTodo.length} items left`;
-};
+let ListTodo = [];
 
 check__icon.addEventListener('click', (event) => {
     if (!check__icon.classList.contains('checked')) {
         check__icon.classList.add('checked');
-        check__icon.children[0].style.visibility = 'visible';
     } else {
         check__icon.classList.remove('checked');
-        check__icon.children[0].style.visibility = 'hidden';
     }
 });
+
+form.addEventListener('submit', (event) => {
+    if (input.value === '') return;
+    
+    // create an array of objects with the inputs
+    ListTodo = ObjectTodo(event);
+    // display the list
+    DisplayTodo(ListTodo);
+    event.preventDefault();
+});
+
+ul.addEventListener('click', (event) => {
+    let CheckTodoList = ListTodo.map(obj => {
+        if (obj.id == event.target.dataset.id) {
+            if (!event.target.classList.contains('checked')) {
+                event.target.classList.add('checked');
+                obj.completed = true;
+            } else {
+                event.target.classList.remove('checked');
+                obj.completed = false;                
+            }
+            obj;
+        }
+       return obj;
+    });
+
+    DisplayTodo(CheckTodoList);
+});
+
+const ObjectTodo = (event) => {
+    let eventChecked = event.target.children[0].children[0];
+    let completed;
+    eventChecked.classList.contains('checked') ? completed = true : completed = false;
+
+    let id = ListTodo.length;
+
+    ListTodo.push({ completed: completed, id: id, text: input.value });
+    input.value = '';   
+
+    return ListTodo;
+};
+
+const DisplayTodo = (ListTodo) => {
+   const htmlString = ListTodo
+            .map(todo => {
+                return `<li>
+                            <span data-id="${todo.id}" class="check__icon ${todo.completed ? 'checked' : ''}">
+                                <img style="visibility: hidden" 
+                                src="app/images/icon-check.svg" alt="icon-check">
+                            </span>
+                            <a href="#">${todo.completed ? 
+                                        `<s style="color: hsl(233, 14%, 35%); font-weight: normal">${todo.text}</s>`: 
+                                        todo.text}
+                                        </a>
+                            <i>
+                                <img data-id="${todo.id}" src="app/images/icon-cross.svg" alt="icon-cross">
+                            </i>
+                        </li>`;
+            }).join('');
+
+    document.getElementById('listTodo').innerHTML = htmlString;
+    
+    UpdateItemLeft();
+};
+
+const UpdateItemLeft = () => {
+    let ListItems = Array.from(ul.children).map(node => node.innerText);
+        let span = desktop__states.querySelector('span');
+        let spanMobile = list__description.querySelector('span');
+
+        span.innerText = `${ListItems.length} items left`;
+        spanMobile.innerText = `${ListItems.length} items left`;
+};
+
+states__list.addEventListener('click', (event) => {
+    switch (event.target.innerText) {
+        case 'Completed':
+            FilterCompleted();
+            break;
+        case 'Active':
+            FilterActive();
+            break;
+        default: 
+            DisplayTodo(ListTodo);
+    }
+});
+
+const FilterCompleted = () => {
+    let FilterTodo = ListTodo.filter(item => item.completed === true);
+
+    DisplayTodo(FilterTodo);
+};
+
+const FilterActive = () => {
+    let FilterTodo = ListTodo.filter(item => item.completed === false);
+
+    DisplayTodo(FilterTodo);
+};
+
+
+
+
 
 
